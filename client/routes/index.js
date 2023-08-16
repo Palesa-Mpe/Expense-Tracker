7ac39355855f04a072f1727be649d001b8ecdcbe
JRvNiekerk <juanr@bbd.co.za>
2023-08-16 16:05:55 +0200

    Added frontend insert expenses page.

--- client/routes/index.js
+++ client/routes/index.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const usersRouter = require('./user');
 
+router.get('/', function(req, res) {
+    res.sendFile("expenses.html", {
+      root: "./pages/",
+    });
+  });
 router.use('/users', usersRouter);
 router.all('*', function(req, res) {res.redirect('/users');});
 
