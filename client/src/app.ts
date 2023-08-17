37881f5e0d8e9990046203e8cf4f0e0b083b0c9a
JRvNiekerk <juanr@bbd.co.za>
2023-08-17 11:21:18 +0200

    Changed from route to serve static
    Removed the distribution folder as well.

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,4 +1,5 @@
 import express, {Express, Request, Response} from "express";
+import path from "path";
 import {ResourceConfig} from './config';
 
 const resourceConfig: ResourceConfig = ResourceConfig;
@@ -8,28 +9,31 @@ const port: string | number = resourceConfig.port;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
-app.use(express.static("./src/public", { extensions: ["html", "css", "png", "svg", 'gif'] }));
+app.use(express.static("./src/public", { extensions: ["css", "png", "svg", 'gif'] }));
+app.use(express.static("./src/views", { extensions: ["html"] }));
 app.use(express.static("./dist/public", { extensions: ["js"] }));
 
 app.get('/', (req: Request, res: Response) => {
-  res.sendFile('login.html', { root: "./src/views" })
+  res.redirect('/login');
 });
 
-// app.get('/register', (req: Request, res: Response) => {
-//   res.sendFile('register.html', { root: "./src/views" })
-// });
+// // app.get('/register', (req: Request, res: Response) => {
+// //   res.sendFile('register.html', { root: "./src/views" })
+// // });
 
-// app.get('/dashboard', (req: Request, res: Response) => {
-//   res.sendFile('dashboard.html', { root: "./src/views" })
-// });
+// // app.get('/dashboard', (req: Request, res: Response) => {
+// //   res.sendFile('dashboard.html', { root: "./src/views" })
+// // });
 
-// app.get('/expensesOverview', (req: Request, res: Response) => {
-//   res.sendFile('analytics.html', { root: "./src/views" })
-// });
+// // app.get('/expensesOverview', (req: Request, res: Response) => {
+// //   res.sendFile('analytics.html', { root: "./src/views" })
+// // });
 
-app.get('/addExpense', (req: Request, res: Response) => {
-  res.sendFile('expenses.html', { root: "./src/views" })
-});
+// app.get('/addExpense', (req: Request, res: Response) => {
+//   console.log("here");
+  
+//   res.sendFile('index.html', { root: "./dist/views/addExpense" })
+// });
 
 app.all('*', (req: Request, res: Response) => {
   res.redirect('/addExpense');
