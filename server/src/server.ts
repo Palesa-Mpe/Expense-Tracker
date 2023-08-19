7d5afc543f591c53f96129f9193095f0a6457b14
Mmasehume <mmasehume@bbd.co.za>
2023-08-20 00:53:20 +0200

    update server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
-import express, {Express} from "express";
+import express, {Express, Request, Response} from "express";
 import {ResourceConfig} from './config';
 import {router} from './routes';
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 import cors from "cors";
@@ -17,13 +18,26 @@ server.use(cors({
 
 server.use(express.urlencoded({extended: true}))
 server.use(express.json());
+server.use(express.static('public'));
+server.use(express.static(path.join(__dirname, '../../client/src/public'), { extensions: ["css", "png", "svg", "gif", "jpg", "jpeg",] }));
+server.use(express.static(path.join(__dirname, '../../client/src/views'), { extensions: ["html"] }));
+server.use(express.static(path.join(__dirname, '../../client/dist/public'), { extensions: ["js"] }));
+server.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../client/src/views/login/index.html'));
+})
 
 
-server.use('/', router);
+// API routes
+server.use('/api/', router);
 server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-server.get('*', function (req, res) {
+
+server.get('/api/', function (req, res) {
   res.redirect('/api-docs');
 })
+
+server.all('*', (req: Request, res: Response) => {
+  res.redirect('/404');
+});
 
 server.listen(port, () => {
   console.log(`http://localhost:${port}`);
